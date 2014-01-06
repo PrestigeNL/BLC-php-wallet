65fdda1d0b3c9f42f1e712e0e7841a1374ff6cab
LoveLeAnon <LoveLeAnon@users.noreply.github.com>
2014-01-05 19:59:13 -0800

    Update transactions.php

--- transactions.php
+++ transactions.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
@@ -30,7 +29,7 @@
 	</head>
 	<body>
 		
-					<h1 id="head">Online BLC wallet</h1>
+					<h1 id="head">Online RBC wallet</h1>
 		
 		<ul id="navigation">
 			<li><a href="wallet.php">Wallet</a></li>
